fdff6db964677ee1a13f03b7311a21f20bfbbafc
Alan Clark <32013626+thatscotdatasci@users.noreply.github.com>
2022-01-02 16:39:45 +0000

    Tweaks to default run parameters

--- src/scse/default_run_parameters/national_grid_default_run_parameters.py
+++ src/scse/default_run_parameters/national_grid_default_run_parameters.py
@@ -5,10 +5,11 @@ from scse.default_run_parameters.core_default_run_parameters import CoreRunParam
 @dataclass
 class _RunParameters(CoreRunParameters):
     # Override certain core parameters
-    run_profile = 'national_grid_profile.json'
+    run_profile = 'national_grid_profile'
     asin_selection = 0  # Use 0 for national grid simulation
     logging_level = 'DEBUG'  # May need to set to critical occassionally
     time_increment = 'half-hourly'
+    time_horizon = 336  # 1 week, if 30 min time increments
 
     # Bespoke parameters
     simulation_logging_level = 'CRITICAL'
